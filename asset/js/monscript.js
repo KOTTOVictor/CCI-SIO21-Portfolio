dcd7d496d5e0e08078fa3dbc4d9248e50b23e07c
KOTTOVictor <alss-sio-slam21-evi@ccicampus.fr>
2022-02-22 13:23:29 +0100

    dernière maj 22/02/22

--- asset/js/monscript.js
+++ asset/js/monscript.js
@@ -5,12 +5,6 @@ let path = new Array("./html/MonParcours.html", "./html/Projet.html", "./html/Ex
 let title = new Array("Mon Parcours", "Mes Projets", "Mes ExperiencesProffessionel" )
 let pied = new Array("#d71b3b", "#4203c9", "#16acea" )
 
-let fillText = new Array("Mon Parcours", "Mes Projets", "Mes ExperiencesProffessionel" )
-
-
-// let description = new Array("Mon Parcours", "Mes Projets", "Mes Experiences Proffessionel" )
-
-
 
 function ChangeSlide(sens) 
 {
@@ -20,22 +14,17 @@ function ChangeSlide(sens)
      if (numero > slide.length - 1)
          numero = 0;
      document.getElementById("slide").src = slide[numero];
-     document.getElementById("slide").fillText = fillText[numero];
      document.getElementById("slidlink").href = path[numero];
      document.getElementById("slide").title = title[numero];
      document.getElementById("pied").style.backgroundColor = pied[numero];
-    //  document.getElementById("slidlink").description = description[numero];  
 }
-// context.fillText("Bonjour",40,40);
-// context.strokeText("Bonjour",40,100);
 
 setInterval("ChangeSlide(1)", 15000);
 
 // Width PETIT PETIT---------------------------------------------------------
 let blockfgpetit = document.getElementById("blockfgpetit");
 let blockfdpetit = document.getElementById("blockfdpetit");
-// let fgpetit = document.getElementById("fgpetit");
-// let fdpetit = document.getElementById("fdpetit");
+
 
 blockfgpetit.style.display = "none";
 
@@ -76,7 +65,6 @@ blockfdmoyen.addEventListener("click", () => {
     fgmoyen.style.display = "block";
 
 })
-
 
 
 
